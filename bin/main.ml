5831b9fdab9b7ffaef2ceceacb4456e06bb37a83
Daniel Velasquez <danivelas4@gmail.com>
2024-05-05 16:16:41 -0400

    trying out the core library M bin/dune M bin/main.ml

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,10 @@
-let () = print_endline "Hello, World!"
+open Core
+
+let () =
+  let filename = "data" in
+  let max_number =
+    let open In_channel in
+    with_file ~binary:false filename
+      ~f:(fold_lines ~init:0 ~f:(fun m s -> Int.(max m @@ of_string s)))
+  in
+  printf "Max number is %s is %d\n" filename max_number
