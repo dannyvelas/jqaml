5f3870ff235edb8c915780cf3dafd9d20361c446
Daniel Velasquez <danivelas4@gmail.com>
2024-05-05 21:36:49 -0400

    bin/main.ml: added tokenize fn

--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,12 @@
 open Core
 
+let tokenize _s = []
+
 let rec repl () =
   let line = In_channel.input_line In_channel.stdin in
   match line with
   | Some x ->
-      print_endline x;
+      let _tokens = tokenize x in
       repl ()
   | None -> ()
 
