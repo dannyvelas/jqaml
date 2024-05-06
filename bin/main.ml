1c02c57c31feb4f9ee58432f435822dc988fbb19
Daniel Velasquez <danivelas4@gmail.com>
2024-05-05 20:14:50 -0400

    playing with Core library M bin/main.ml

--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,5 @@
 open Core
 
 let () =
-  let filename = "data" in
-  let max_number =
-    let open In_channel in
-    with_file ~binary:false filename
-      ~f:(fold_lines ~init:0 ~f:(fun m s -> Int.(max m @@ of_string s)))
-  in
-  printf "Max number is %s is %d\n" filename max_number
+  In_channel.iter_lines In_channel.stdin ~f:(fun l ->
+      printf "%d\n" (String.length l))
