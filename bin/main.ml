bc3177a03a13a86adc4316530d1c5fa9556d5996
Daniel Velasquez <danivelas4@gmail.com>
2024-05-05 22:55:10 -0400

    bin/main.ml: first basic skeleton of main pattern matching function

--- bin/main.ml
+++ bin/main.ml
@@ -18,21 +18,39 @@ let peek state = state.input.[state.pos]
 let next state =
   let next_byte = peek state in
   let new_state = { state with pos = state.pos + 1 } in
-  (next_byte, new_state)
+  (new_state, next_byte)
 
 let is_at_end state = state.pos >= String.length state.input
 let append_token state token = { state with tokens = state.tokens @ token }
 
-let update_state state byte =
-  match byte with '.' -> append_token state { tokenType = DOT; lexeme = byte }
+type err = { message : string; value : char }
 
-let tokenize s ?(state = { input = s; start = 0; pos = 0; tokens = [] }) =
-  if is_at_end state then
-    state.tokens
-  else
-    let state = { state with start = state.pos } in
-    let state, byte = next state in
-    tokenize (update_state state byte)
+let update_state state byte =
+  match byte with
+  | '.' ->
+      Ok
+        (append_token state
+           [ { tokenType = DOT; lexeme = Char.to_string byte } ])
+  | 'a' .. 'z' | 'A' .. 'Z' -> Ok state
+  | '0' .. '9' -> Ok state
+  | '|' -> Ok state
+  | '"' -> Ok state
+  | '[' -> Ok state
+  | ']' -> Ok state
+  | _ -> Error { message = "did not recognize byte"; value = byte }
+
+let tokenize s =
+  let rec tokenize state =
+    if is_at_end state then
+      Ok state.tokens
+    else
+      let state = { state with start = state.pos } in
+      let state, byte = next state in
+      match update_state state byte with
+      | Error err -> Error err
+      | Ok state -> tokenize state
+  in
+  tokenize { input = s; start = 0; pos = 0; tokens = [] }
 
 let rec repl () =
   let line = In_channel.input_line In_channel.stdin in
