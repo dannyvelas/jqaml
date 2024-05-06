1ac89df1d7edff8c438e9489c4181001978a1882
Daniel Velasquez <danivelas4@gmail.com>
2024-05-05 21:19:41 -0400

    bin/main.ml: implemented the same with recursion

--- bin/main.ml
+++ bin/main.ml
@@ -1,9 +1,11 @@
 (*open Core*)
 
-let () =
+let rec repl lines =
   try
-    while true do
-      let line = read_line () in
-      print_endline line
-    done
-  with End_of_file -> ()
+    let line = read_line () in
+    repl (line :: lines)
+  with End_of_file -> List.rev lines
+
+let () =
+  let lines = repl [] in
+  List.iter (fun line -> print_endline line) lines
