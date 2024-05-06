21a3aed3497cf3fa387c45b66c2e343d3392900c
Daniel Velasquez <danivelas4@gmail.com>
2024-05-05 21:24:46 -0400

    bin/main.ml: implemented the same with core library

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,5 @@
-(*open Core*)
-
-let rec repl lines =
-  try
-    let line = read_line () in
-    repl (line :: lines)
-  with End_of_file -> List.rev lines
+open Core
 
 let () =
-  let lines = repl [] in
-  List.iter (fun line -> print_endline line) lines
+  let lines = In_channel.input_lines In_channel.stdin in
+  List.iter lines ~f:(fun line -> print_endline line)
