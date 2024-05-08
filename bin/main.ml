722f1c403da0db6babd191183c447ce3ed4b0410
Daniel Velasquez <danivelas4@gmail.com>
2024-05-07 21:08:17 -0400

    bin/main.ml: made repl be defined locally

--- bin/main.ml
+++ bin/main.ml
@@ -51,12 +51,13 @@ let tokenize s =
   in
   tokenize { input = s; start = 0; pos = 0; tokens = [] }
 
-let rec repl () =
-  let line = In_channel.input_line In_channel.stdin in
-  match line with
-  | Some x ->
-      let _tokens = tokenize x in
-      repl ()
-  | None -> ()
-
-let () = repl ()
+let () =
+  let rec repl () =
+    let line = In_channel.input_line In_channel.stdin in
+    match line with
+    | None -> ()
+    | Some x ->
+        let _tokens = tokenize x in
+        repl ()
+  in
+  repl ()
