06f2c992ddc343ba2e5a80bf7f5bacc1501fe043
Daniel Velasquez <danivelas4@gmail.com>
2024-05-08 09:02:52 -0400

    bin/main.ml: playing a little with option.fold as in scala

--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,10 @@
-open Core
+(*open Core*)
 
 let repl inputch =
   let lexbuf = Lexing.from_channel inputch in
   try
     let a = Parser.main Lexer.token lexbuf in
-    List.iter ~f:(fun a -> printf "%d\n" a) a
+    Core.List.iter ~f:(fun a -> Core.printf "%d\n" a) a
   with
   | Lexer.SyntaxError msg -> Printf.eprintf "%s%!" msg
   | Parser.Error ->
@@ -13,13 +13,13 @@ let repl inputch =
       In_channel.close inputch
 
 let () =
-  let argv = Sys.get_argv () in
+  let argv = Core.Sys.get_argv () in
   let filename =
     match argv.(1) with exception Invalid_argument _ -> None | x -> Some x
   in
   let inputch =
-    match filename with
-    | None -> In_channel.stdin
-    | Some filename -> In_channel.create filename
+    Option.fold ~none:In_channel.stdin
+      ~some:(fun f -> Core.In_channel.create f)
+      filename
   in
   repl inputch
