7f664771c8c3d5ed68211bedb45e0eeffe821abd
Daniel Velasquez <danivelas4@gmail.com>
2024-05-08 08:45:21 -0400

    bin/main.ml: added support for both repl and file provided as arg

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,7 @@
 open Core
 
-let () =
-  let inx = In_channel.create "hi.calc" in
-  let lexbuf = Lexing.from_channel inx in
+let repl inputch =
+  let lexbuf = Lexing.from_channel inputch in
   try
     let a = Parser.main Lexer.token lexbuf in
     List.iter ~f:(fun a -> printf "%d\n" a) a
@@ -11,4 +10,16 @@ let () =
   | Parser.Error ->
       Printf.eprintf "At offset %d: syntax error.\n%!"
         (Lexing.lexeme_start lexbuf);
-      In_channel.close inx
+      In_channel.close inputch
+
+let () =
+  let argv = Sys.get_argv () in
+  let filename =
+    match argv.(1) with exception Invalid_argument _ -> None | x -> Some x
+  in
+  let inputch =
+    match filename with
+    | None -> In_channel.stdin
+    | Some filename -> In_channel.create filename
+  in
+  repl inputch
