87fecdd71f6c460cd1f6fb9a5e6793165f954352
Daniel Velasquez <danivelas4@gmail.com>
2024-05-11 22:42:23 -0400

    bin/main.ml: removed unused code

--- bin/main.ml
+++ bin/main.ml
@@ -49,41 +49,6 @@ let run inputch =
         (Lexing.lexeme_start lexbuf);
       In_channel.close inputch
 
-(*
-let load x = match x with
-  | PIPE -> "PIPE"
-  | PERIOD -> "PERIOD"
-  | SEMI -> "SEMI"
-  | COMMA -> "COMMA"
-  | COLON -> "COLON"
-  | EQUAL -> "EQUAL"
-  | DOLLAR -> "DOLLAR"
-  | RECURSE -> "RECURSE"
-  | LBRACKET -> "LBRACKET"
-  | RBRACKET -> "RBRACKET"
-  | LPAREN -> "LPAREN"
-  | RPAREN -> "RPAREN"
-  | LCURLY -> "LCURLY"
-  | RCURLY -> "RCURLY"
-  | PLUS -> "PLUS"
-  | MINUS -> "MINUS"
-  | MUL -> "MUL"
-  | DIV -> "DIV"
-  | AND -> "AND"
-  | OR -> "OR"
-  | NOT -> "NOT"
-  | INDEX -> "INDEX"
-  | NULL -> "NULL"
-  | NUMBER -> "NUMBER" _CONSTANT
-  | STRING -> "STRING" _CONSTANT
-  | TRUE -> "TRUE"
-  | FALSE -> "FALSE"
-  | DEF -> "DEF"
-  | REDUCE -> "REDUCE"
-  | FOREACH -> "FOREACH"
-  | EOF -> "EOF"
-  *)
-
 let () =
   let argv = Sys.get_argv () in
   let filename =
