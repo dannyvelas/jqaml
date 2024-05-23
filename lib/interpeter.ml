35bdd4db86d9285141eeea21e6681177909e4a80
Daniel Velasquez <danivelas4@gmail.com>
2024-05-23 09:05:57 -0400

    lib/interpeter.ml: temporarily trying to return an `interpret_result` value from my top-level `interpret` function

--- lib/interpeter.ml
+++ lib/interpeter.ml
@@ -1,6 +1,6 @@
-type interpret_result = Number of int | Null | Boolean of bool
+type interpret_result = Number of int | Null | Boolean of bool | Identity | Index
 
-let rec interpret_internal (query : Cst.query) : interpret_result =
+let rec interpret_internal (query : Cst.query): interpret_result =
   match query with
   | Cst.Empty -> Null
   | Cst.Term term -> (
@@ -8,6 +8,14 @@ let rec interpret_internal (query : Cst.query) : interpret_result =
       | Null -> Null
       | True -> Boolean true
       | False -> Boolean false
-      | Identity _ -> Null (* return input as is *)
-      | Recurse -> Null (* recurse through input *)
-      | Index _ -> Null (*index input*))
+      | Identity _ -> Identity
+      | Recurse -> Identity
+          (* recurse through input: not implemented yet bc objects are not in grammar  *)
+      | Index _ ->
+          Index
+      | Number num -> Number num)
+    | Cst.JoinedQuery term op query -> (
+      let term_result = interpret_internal @@ Cst.query term in
+      let query_result = interpret_internal @@ query in
+
+  )
