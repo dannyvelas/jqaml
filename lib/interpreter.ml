ca934b6a1b2fa7e026baa52f3b09fd9eba0c9ea1
Daniel Velasquez <danivelas4@gmail.com>
2024-05-24 14:57:25 -0400

    lib/interpreter.ml: removed result type from interpreter

--- lib/interpreter.ml
+++ lib/interpreter.ml
@@ -1,40 +1,38 @@
 let ( let* ) = Result.bind
 
-let factor_to_int (factor : Cst.factor) : (int, string) result =
-  match factor with
-  | Cst.Null -> Ok 0
-  | Cst.Number number -> Ok number
-  | Cst.True | Cst.False -> Error "cannot perform arithmetic on boolean"
-  | _ -> Error "unimplemented"
+exception TypeError of string
 
-let rec interpret (query : Cst.query) : (Cst.factor, string) result =
+let check_type (factor : Cst.factor) =
+  match factor with
+  | Cst.True | Cst.False ->
+      raise (TypeError "cannot perform arithmetic on boolean")
+  | _ -> ()
+
+let rec interpret (query : Cst.query) : Cst.factor =
   match query with
-  | Cst.Empty -> Ok Cst.Null
+  | Cst.Empty -> Cst.Null
   | Cst.Expr expr -> (
       match expr with
-      | Cst.Term term -> ( match term with Factor factor -> Ok factor)
+      | Cst.Term term -> ( match term with Factor factor -> factor)
       | Cst.Arithmetic (expr, _, term) ->
-          let* lh_factor = Cst.Expr expr |> interpret in
-          let* rh_factor = Cst.Expr (Cst.Term term) |> interpret in
-          let lh_factor', rh_factor' =
-            (factor_to_int lh_factor, factor_to_int rh_factor)
-          in
-          Ok Cst.Null)
+          let lh_factor = Cst.Expr expr |> interpret in
+          let rh_factor = Cst.Expr (Cst.Term term) |> interpret in
+          check_type lh_factor;
+          check_type rh_factor;
+          Cst.Null)
   | Cst.JoinedQuery (expr, _, query) -> (
       let lh_factor = interpret @@ Cst.Expr expr in
       let rh_factor = interpret query in
       match (lh_factor, rh_factor) with
-      | Error err, _ -> Error err
-      | _, Error err -> Error err
       (* pipe operator *)
-      | lh_factor, Ok (Cst.Identity _) -> lh_factor
-      | lh_factor, Ok Cst.Recurse ->
+      | lh_factor, Cst.Identity _ -> lh_factor
+      | lh_factor, Cst.Recurse ->
           (* make recurse behave like identity for now *)
           lh_factor
-      | lh_factor, Ok (Cst.Index _) ->
+      | lh_factor, Cst.Index _ ->
           (* make Index behave like identitiy as objects are not implemented yet *)
           lh_factor
-      | lh_factor, Ok (Cst.BracketSuffix _) ->
+      | lh_factor, Cst.BracketSuffix _ ->
           (* make BracketSuffix behave like identity as strings/arrays are not implement yet *)
           lh_factor
       | _, rh_factor ->
