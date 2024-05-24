98e3ee2f88b33561a02b909b68f05b016d7bd3d8
Daniel Velasquez <danivelas4@gmail.com>
2024-05-24 14:50:26 -0400

    lib/interpreter.ml: playing around with making interpret fn return result

--- lib/interpreter.ml
+++ lib/interpreter.ml
@@ -1,21 +1,42 @@
-let rec interpret (query : Cst.query) : Cst.term =
+let ( let* ) = Result.bind
+
+let factor_to_int (factor : Cst.factor) : (int, string) result =
+  match factor with
+  | Cst.Null -> Ok 0
+  | Cst.Number number -> Ok number
+  | Cst.True | Cst.False -> Error "cannot perform arithmetic on boolean"
+  | _ -> Error "unimplemented"
+
+let rec interpret (query : Cst.query) : (Cst.factor, string) result =
   match query with
-  | Cst.Empty -> Null
-  | Cst.Term term -> term
-  | Cst.JoinedQuery (term, _, query) -> (
-      (* for now, operator is ignored bc it can only be PIPE *)
-      let lh_term = term in
-      let rh_term = interpret query in
-      match (lh_term, rh_term) with
-      | lh_term, Identity _ -> lh_term
-      | lh_term, Recurse ->
-          lh_term (* make recurse behave like identity for now *)
-      | lh_term, Index _ ->
-          lh_term
+  | Cst.Empty -> Ok Cst.Null
+  | Cst.Expr expr -> (
+      match expr with
+      | Cst.Term term -> ( match term with Factor factor -> Ok factor)
+      | Cst.Arithmetic (expr, _, term) ->
+          let* lh_factor = Cst.Expr expr |> interpret in
+          let* rh_factor = Cst.Expr (Cst.Term term) |> interpret in
+          let lh_factor', rh_factor' =
+            (factor_to_int lh_factor, factor_to_int rh_factor)
+          in
+          Ok Cst.Null)
+  | Cst.JoinedQuery (expr, _, query) -> (
+      let lh_factor = interpret @@ Cst.Expr expr in
+      let rh_factor = interpret query in
+      match (lh_factor, rh_factor) with
+      | Error err, _ -> Error err
+      | _, Error err -> Error err
+      (* pipe operator *)
+      | lh_factor, Ok (Cst.Identity _) -> lh_factor
+      | lh_factor, Ok Cst.Recurse ->
+          (* make recurse behave like identity for now *)
+          lh_factor
+      | lh_factor, Ok (Cst.Index _) ->
           (* make Index behave like identitiy as objects are not implemented yet *)
-      | lh_term, BracketSuffix _ ->
-          lh_term
+          lh_factor
+      | lh_factor, Ok (Cst.BracketSuffix _) ->
           (* make BracketSuffix behave like identity as strings/arrays are not implement yet *)
-      | _, rh_term ->
-          rh_term (* if right hand term is just a hard-coded value, return it *)
-      )
+          lh_factor
+      | _, rh_factor ->
+          (* if right hand factor is just a hard-coded value, return it *)
+          rh_factor)
